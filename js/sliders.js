8e3952ce8927038f17609d395b4029a205e31ce4
Aliaksei Karzhou <alexius90e@gmail.com>
2024-01-31 22:12:39 +0300

    feat: added five-elements

--- js/sliders.js
+++ js/sliders.js
@@ -20,3 +20,21 @@ const mainSwiper = new Swiper(".main-slider .swiper", {
     el: ".main-slider .swiper-pagination",
   },
 });
+
+const fiveElementsSwiper = new Swiper(".five-elements .swiper", {
+  spaceBetween: 24,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.5,
+    },
+    464: {
+      slidesPerView: 2.5,
+    },
+    576: {
+      slidesPerView: 3.5,
+    },
+    744: {
+      slidesPerView: 5,
+    },
+  },
+});
